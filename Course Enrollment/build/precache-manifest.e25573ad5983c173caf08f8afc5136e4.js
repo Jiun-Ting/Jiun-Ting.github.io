f5ce489f1ac579e054efe6e239ebe49a7d4cf0e3
Jiun-Ting <R03323057@ntu.edu.tw>
2021-02-07 21:22:44 -0600

    test

---
+++ Course Enrollment/build/precache-manifest.e25573ad5983c173caf08f8afc5136e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e83953047d4376ec9724a3c956f8856f",
+    "url": "/Course%20Enrollment/index.html"
+  },
+  {
+    "revision": "ddb175b2423f3f9a706a",
+    "url": "/Course%20Enrollment/static/css/main.e6d6dde8.chunk.css"
+  },
+  {
+    "revision": "947260334a4492c2d98c",
+    "url": "/Course%20Enrollment/static/js/2.94b3d934.chunk.js"
+  },
+  {
+    "revision": "f86a4e17d3cb58f717d9f416937db9aa",
+    "url": "/Course%20Enrollment/static/js/2.94b3d934.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddb175b2423f3f9a706a",
+    "url": "/Course%20Enrollment/static/js/main.e3aba65a.chunk.js"
+  },
+  {
+    "revision": "93b22165cf1ff7ed65a9",
+    "url": "/Course%20Enrollment/static/js/runtime-main.dde0e14d.js"
+  }
+]);
